43623caede4b28a823b3cb16d4f4d7635d148983
Lennox Kabo <borelennox@gmail.com>
2024-04-06 01:31:51 +0300

    adding bootstrap and jquery

--- member-home.php
+++ member-home.php
@@ -22,6 +22,8 @@ if (!$member) {
     exit('Member details not found.');
 }
 ?>
+<head>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
 <style>
     /* Custom styles for the loan application page */
@@ -42,6 +44,8 @@ if (!$member) {
         width: 100%;
     }
 </style>
+</head>
+
 
 <div class="container-fluid">
     <div class="row">
@@ -67,7 +71,9 @@ if (!$member) {
         </div>
     </div>
 </div>
-
+<!-- jQuery and Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script>
     $(document).ready(function() {
         $('#loan-application').submit(function(e) {
